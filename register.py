c1b3ca702705bfb066f9d8a42f90ba1e6cb84769
yumik <crab.ditch@gmail.com>
2025-07-17 10:10:02 +0800

    README edit

--- register.py
+++ register.py
@@ -9,7 +9,7 @@ with app.app_context():
     print("帳密新增完成")
     db.session.commit()
 
-#id=1, username="yumik", pwd="3310"
+#id=1, username="yumik", pwd="3310"   ### teacher
 #id=2, username="Amy", password="1111"
 #id=3, username="Mike", password="1314"
 #id=4, username="Vivi", password="7879"
